d3433c730d451bc36e803d3600456e0888797c77
Mrinal Patra <mrinalpatra7@gmail.com>
2020-08-19 19:59:31 +0530

    added logic to apply promotions on all cart items

---
+++ src/PromotionEngine/Models/MarkedItem.cs
@@ -0,0 +1,12 @@
+﻿using PromotionEngine.Models;
+using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    public class MarkedItem
+    {
+        public Item Item { get; set; }
+        public Dictionary<string, bool> MarkedBuys { get; set; }
+        public Dictionary<string, float> MarkedGets { get; set; }
+    }
+}
--- src/PromotionEngine/PromoCalculator.cs
+++ src/PromotionEngine/PromoCalculator.cs
@@ -8,6 +8,45 @@ namespace PromotionEngine
     {
         public object ApplyPromos(List<Promotion> promotions, List<Item> cartItems)
         {
+            List<MarkedItem> markedItems = new List<MarkedItem>();
+
+            foreach (var item in cartItems)
+            {
+                MarkedItem mItem = new MarkedItem()
+                {
+                    Item = item,
+                    MarkedBuys = new Dictionary<string, bool>(),
+                    MarkedGets = new Dictionary<string, float>()
+                };
+                markedItems.Add(mItem);
+            }
+
+            foreach (var promo in promotions)
+            {
+                markedItems = ApplyPromo(promo, markedItems);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private List<MarkedItem> ApplyPromo(Promotion promo, List<MarkedItem> markedItems)
+        {
+            var appliedBuysItems = applyBuys(promo, markedItems);
+
+            markedItems = appliedBuysItems;
+            markedItems = applyGets(promo, markedItems);
+
+
+            return markedItems;
+        }
+
+        private List<MarkedItem> applyGets(Promotion promo, List<MarkedItem> markedItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<MarkedItem> applyBuys(Promotion promo, List<MarkedItem> markedItems)
+        {
             throw new NotImplementedException();
         }
     }

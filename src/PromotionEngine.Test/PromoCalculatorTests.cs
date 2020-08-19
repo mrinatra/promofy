82f248dcd275a5b02de5955bc93f95c71f6a99ba
Mrinal Patra <mrinalpatra7@gmail.com>
2020-08-19 19:12:08 +0530

    added additional test cases

--- src/PromotionEngine.Test/PromoCalculatorTests.cs
+++ src/PromotionEngine.Test/PromoCalculatorTests.cs
@@ -27,6 +27,38 @@ namespace PromotionEngine.Test
             Assert.Equal(modifiedCart.TotalPrice, modifiedCart.TotalOffPrice);
         }
 
+        [Theory]
+        [InlineData("itemsForScenarioB.json")]
+        public void Test_When_AllPromos_Are_Applied_It_Should_Apply_To_All_Eligble_Items_Scenario_B_Two_Eligible_Items(string itemsFileName)
+        {
+            var promotions = GetAllPromotions();
+
+            var cartItems = GetCartItems(itemsFileName);
+
+            var promoEngine = new PromoCalculator();
+
+            dynamic modifiedCart = promoEngine.ApplyPromos(promotions, cartItems);
+
+            Assert.Equal(420, modifiedCart.TotalPrice);
+            Assert.Equal(370, modifiedCart.TotalOffPrice);
+        }
+
+        [Theory]
+        [InlineData("itemsForScenarioC.json")]
+        public void Test_When_AllPromos_Are_Applied_It_Should_Apply_To_All_Eligble_Items_Scenario_C_Three_Eligible_Items(string itemsFileName)
+        {
+            var promotions = GetAllPromotions();
+
+            var cartItems = GetCartItems(itemsFileName);
+
+            var promoEngine = new PromoCalculator();
+
+            dynamic modifiedCart = promoEngine.ApplyPromos(promotions, cartItems);
+
+            Assert.Equal(335, modifiedCart.TotalPrice);
+            Assert.Equal(280, modifiedCart.TotalOffPrice);
+        }
+
         private object GetCartItems(string itemsFileName)
         {
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "TestData", itemsFileName);

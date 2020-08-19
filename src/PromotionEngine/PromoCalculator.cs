57e15a62392ef396c3e10cc09447f2e5167e3639
Mrinal Patra <mrinalpatra7@gmail.com>
2020-08-19 20:19:01 +0530

    added logic to check if buy criteria satisfied for each promotion for all items

--- src/PromotionEngine/PromoCalculator.cs
+++ src/PromotionEngine/PromoCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using PromotionEngine.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine
 {
@@ -31,21 +32,56 @@ namespace PromotionEngine
 
         private List<MarkedItem> ApplyPromo(Promotion promo, List<MarkedItem> markedItems)
         {
-            var appliedBuysItems = applyBuys(promo, markedItems);
+            var appliedBuysItems = ApplyBuys(promo, markedItems);
 
-            markedItems = appliedBuysItems;
-            markedItems = applyGets(promo, markedItems);
+            if (!appliedBuysItems.applied)
+                return markedItems;
 
+            markedItems = appliedBuysItems.items;
+
+            markedItems = ApplyGets(promo, markedItems);
 
             return markedItems;
         }
 
-        private List<MarkedItem> applyGets(Promotion promo, List<MarkedItem> markedItems)
+        private (bool applied, List<MarkedItem> items) ApplyBuys(Promotion promo, List<MarkedItem> markedItems)
         {
-            throw new NotImplementedException();
+            bool applied = true;
+
+            foreach (var buy in promo.Buy)
+            {
+                var appliedBuy = applyBuy(buy, promo, markedItems);
+                applied = applied && appliedBuy.applied;
+            }
+            return (applied, markedItems);
         }
 
-        private List<MarkedItem> applyBuys(Promotion promo, List<MarkedItem> markedItems)
+        private (bool applied,List<MarkedItem> items) applyBuy(Buys buy, Promotion promo, List<MarkedItem> markedItems)
+        {
+            List<MarkedItem> matchedItems = new List<MarkedItem>();
+
+            var skuList = markedItems.Where(i => i.Item.Sku == buy.Sku).Select(i => i).ToList();
+
+            if (skuList == null || skuList.Count == 0)
+            {
+                return (false, markedItems);
+            }
+
+            foreach (var sku in markedItems)
+            {
+                if (sku.Item.Sku == buy.Sku)
+                {
+                    if (sku.Item.Quantity < buy.Quantity)
+                    {
+                        return (false, markedItems);
+                    }
+                    sku.MarkedBuys[promo.Id] = true;
+                }
+            }
+            return (true, skuList);
+        }
+
+        private List<MarkedItem> ApplyGets(Promotion promo, List<MarkedItem> markedItems)
         {
             throw new NotImplementedException();
         }
